ff873b82e0c758d96cc3a7ca3b59a846f88aa9a9
Camila Velez <cvelezg13@eafit.edu.co>
2024-05-10 20:43:56 -0500

    tarea 3 desde gitbash

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        imprimir();
+    }
+    static void imprimir(){
+        System.out.println("Assignment3 by Camila Velez");
+    }
+}
